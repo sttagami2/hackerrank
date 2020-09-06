71368f8d7a6f25bf33914472a8cb4177108de834
sttagami2 <sttagami2@gmail.com>
2020-09-06 17:16:45 +0900

    finish conditional statements in c

---
+++ c/challenge/conditional_statements_in_c/conditional_statements_in_c.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void print_number(n) {
+    if (n == 0)
+        printf("zero");
+    else if (n == 1)
+        printf("one");
+    else if (n == 2)
+        printf("two");
+    else if (n == 3)
+        printf("three");
+    else if (n == 4)
+        printf("four");
+    else if (n == 5)
+        printf("five");
+    else if (n == 6)
+        printf("six");
+    else if (n == 7)
+        printf("seven");
+    else if (n == 8)
+        printf("eight");
+    else if (n == 9)
+        printf("nine");
+    else
+        printf("Greater than 9");
+}
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    if (n < 0)
+        printf("Error");
+    else if (0 <= n)
+        print_number(n);
+
+    return 0;
+}
